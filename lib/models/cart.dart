040460dd92f7c2598440afb07d643197a2ada31e
gflexx <glennenri24@gmail.com>
2025-09-03 18:23:07 +0300

    Updated product detail UI

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -5,32 +5,37 @@ class Cart extends ChangeNotifier {
   // list items on sale
   List<Item> itemlist = [
     Item(
-      name: 'Cereals',
-      description: 'sth',
+      name: 'Popcorn Maize',
+      description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
       price: 200,
       imageUrl: 'images/cereals.png',
     ),
     Item(
-      name: 'Fruits',
-      description: 'sth',
+      name: 'Citrus Banana Fruit Bundle',
+      description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
       price: 300,
       imageUrl: 'images/fruits.png',
     ),
     Item(
-      name: 'Tubers',
-      description: 'sth',
+      name: 'Zuchini',
+      description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
       price: 300,
       imageUrl: 'images/greens.png',
     ),
     Item(
-      name: 'Vegies',
-      description: 'sth',
+      name: 'Broccoli Cabbage Pack',
+      description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
       price: 150,
       imageUrl: 'images/vegies.png',
     ),
     Item(
-      name: 'Root Vegies',
-      description: 'sth',
+      name: 'Carrot Special Pack',
+      description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
       price: 400,
       imageUrl: 'images/root_vegies.png',
     ),
@@ -54,16 +59,14 @@ class Cart extends ChangeNotifier {
   }
 
   //  add item to cart
-  void addToCart(Item item){
+  void addToCart(Item item) {
     userCartItems.add(item);
     notifyListeners();
   }
 
   // remove item from cart
   void removeFromCart(Item item) {
-    userCartItems.remove(item);  
+    userCartItems.remove(item);
     notifyListeners();
   }
-
-
 }
--- lib/pages/product_view_page.dart
+++ lib/pages/product_view_page.dart
@@ -1,29 +1,82 @@
 import 'package:flutter/material.dart';
 import 'package:minimal_ecommerce_app/models/item.dart';
+import 'package:provider/provider.dart';
 
-class ProductViewPage extends StatelessWidget {
+class ProductViewPage extends StatefulWidget {
   final Item item;
   const ProductViewPage({super.key, required this.item});
+
+  @override
+  State<ProductViewPage> createState() => _ProductViewPageState();
+}
+
+class _ProductViewPageState extends State<ProductViewPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Products'),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      backgroundColor: Colors.grey[300],
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 350,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
+    return Consumer(
+      builder: (context, value, child) => Scaffold(
+        appBar: AppBar(
+          title: Text('Product Details'),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        backgroundColor: Colors.grey[300],
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.asset(widget.item.imageUrl, fit: BoxFit.cover),
+                ),
               ),
-              child: Image.asset(item.imageUrl, fit: BoxFit.cover),
-            ),
-          ],
+              SizedBox(height: 20),
+              // name and price
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      widget.item.name,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                      ),
+                    ),
+                    Text(
+                      '${widget.item.price} Kshs.',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 20),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // description
+                    Text(
+                      "Description",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.start,
+                    ),
+                    SizedBox(height: 20),
+
+                    Text(widget.item.description),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
